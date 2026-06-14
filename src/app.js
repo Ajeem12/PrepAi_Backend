3cc21b605d07cb8f9991e335c0889024bbc7b361
Ajeem Beg <138427053+Ajeem12@users.noreply.github.com>
2026-06-14 12:53:23 +0530

    change cors

--- src/app.js
+++ src/app.js
@@ -1,27 +1,27 @@
-const express = require("express")
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://prep-ai-backend-phi.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://prep-ai-frontend-jet.vercel.app/login",
+    ],
     credentials: true,
   }),
 );
 
 /* require all the routes here */
-const authRouter = require("./routes/auth.routes")
-const interviewRouter = require("./routes/interview.routes")
-
+const authRouter = require("./routes/auth.routes");
+const interviewRouter = require("./routes/interview.routes");
 
 /* using all the routes here */
-app.use("/api/auth", authRouter)
-app.use("/api/interview", interviewRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/interview", interviewRouter);
 
-
-
-module.exports = app
+module.exports = app;
