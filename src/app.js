4ad40cf762ff1dd7c817b32aba9e311d994a810e
Ajeem Beg <138427053+Ajeem12@users.noreply.github.com>
2026-06-16 14:02:27 +0530

    fix: update CORS configuration to use the correct frontend URL

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://prep-ai-frontend-jet.vercel.app/login",
+      "https://prepai-frontend-0mo8.onrender.com/",
     ],
     credentials: true,
   }),
